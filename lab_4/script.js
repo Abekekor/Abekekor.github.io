ac5bab1af7a1429f1c2a7de74507b9b64f596397
Abylaikhan Makhmed <makhmedabilai.2004@gmail.com>
2023-02-25 22:19:51 +0600

    lab

--- lab_4/script.js
+++ lab_4/script.js
@@ -1,52 +1,92 @@
 let Airpods = [
     {
-        "name": "AirPods2",
-        "production": "U.S.A.",
-        "price": 60000
+        version: "AirPods2",
+        production: "U.S.A.",
+        price: 60000,
+        
+        
     },
     {
-        "name": "Airpods3",
-        "production": "U.S.A.",
-        "price": 90000,
+        version: "Airpods3",
+        production: "U.S.A.",
+        price: 90000,
+        
+        
     },
     {
-        "name": "AirPodsPro",
-        "production": "U.S.A",
-        "price": 110000,
+        version: "AirPodsPro",
+        production: "U.S.A",
+        price: 110000,
+        
     },
     {
-        "name": "AirPodsPro2",
-        "production": "U.S.A",
-        "price": 140000,
+        version: "AirPodsPro2",
+        production: "U.S.A",
+        price: 140000,
+        
     },
 ];
 let sum = 0;
 
+
 function avg(Airpods){ 
     let sum = 0; 
     for (let i = 0; i < Airpods.length; i++) { 
-        sum += Airpods[i]['price']; 
+        sum += Airpods[i]["price"]; 
     } 
     return sum / Airpods.length; 
 } 
 
-
-let Airpods_1 = {
-    "name": "AirPods",
-    "country": "U.S.A",
-    "price": 100000
-}
-
-
-Airpods.push(Airpods_1)
-
 for (let i = 0; i < Airpods.length; i++) {
-    document.write("<h5>Airpods: " + Airpods[i]["name"] + "</h5>");
+    document.write("<h5>Airpods: " + Airpods[i]["version"] + "</h5>");
     document.write("<h5>Production: " + Airpods[i]["production"] + "</h5>");
     document.write("<h5>Price: " + Airpods[i]["price"] + "</h5>");
+    document.write("&nbsp;")
     document.writeln("")
+    
 }
-
 document.write("<h5>Average duration: " + avg(Airpods) + " price</h5>")
 
+function createAirPods (version,production,price,color,features) { 
+    return { 
+      version: version, 
+      production: production, 
+      price: price, 
+      color: color, 
+      features: features, 
+      getAirPodsInfo: function() { 
+        return '${this.color} ${this.price} ${this.version} ${this.production}'; 
+      } 
+    }; 
+  } 
+   
+  function createAirPodsArray() { 
+    const airp = [ 
+      createAirPods("AirPods Max", "U.S.A", 300000, "black", ["noise suppresion", "spatial audio", "Bluetooth"]), 
+      
+    ]; 
+   
+    return airp; 
+  } 
+   
+  console.log(createAirPodsArray());
 
+
+
+//function airpodsarray(version,production,price){
+//    let object={
+//        version: version,
+//        production: production,
+//        price: price,
+//    }
+ //   return object;
+//}
+//myObject.forEach(Airpods => {
+ //   aboHTML += `
+  //   <div class="airpods">
+  //    <p>${airpods.make}</p>
+  //    <h3>${airpods.model}</h3>
+  //    <p>$${airpods.price}</p>
+  //   </div>
+ //   `;
+ //});
